736423e45c5441ff68a1b285d042b3a740fa8a3d
Waxes27 <nceba.dumasi@gmail.com>
2021-06-01 14:49:33 +0000

    added Help instruction

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ base = "Data"
 def get_folders():
     tmp = []
     for dir, folder, files in os.walk(base):
-        if len(folder) != 0 and sys.argv[1] in dir.lower():
-            tmp.append(f"{dir}/{folder}")
+        try:
+            if len(folder) != 0 and sys.argv[1] in dir.lower():
+                tmp.append(f"{dir}/{folder}")
+        except IndexError as e:
+            get_help(e)
+            exit()
     return tmp
+
+def get_help(error):
+    if IndexError:
+        print("Missing parameter python3 main.py [FOLDER]")
+    
 
 def main():
     
