edf9d45b842a81782d15aa852d8deee9522cc9d7
Waxes27 <nceba.dumasi@gmail.com>
2021-06-01 14:31:07 +0000

    base main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import os
+import sys
 
 base = "Data"
 
 
 def get_folders():
+    tmp = []
     for dir, folder, files in os.walk(base):
-        if len(folder) != 0:
-            print(f"{dir}/{folder}")
-
+        if len(folder) != 0 and sys.argv[1] in dir.lower():
+            tmp.append(f"{dir}/{folder}")
+    return tmp
 
 def main():
-    get_folders()
+    
+    requested_folders = get_folders()
+
+    print(requested_folders)
 
 
 main()
